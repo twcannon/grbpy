76920757d8018f32af83d43d1c1602da1a6cf72a
twcannon <tcannon.mail@gmail.com>
2019-07-20 12:24:48 -0400

    initial commit. setup works new file:   .gitignore new file:   README.md new file:   grbpy/__init__.py new file:   grbpy/burst.py new file:   setup.py

---
+++ README.md
@@ -0,0 +1,1 @@
+pip install git+https://github.com/jladan/package_demo.git#egg=measurements
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+# read the contents of markdown README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='grbpy',
+    url='https://github.com/gamma-ray-burst/grbpy',
+    author='Thomas Cannon',
+    author_email='cannontw@g.cofc.edu',
+    packages=['grbpy'],
+    install_requires=['numpy'],
+    version='0.1',
+    license='MIT',
+    description='An example of a python package from pre-existing code',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
