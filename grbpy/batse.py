a49bf6af2f8acd71e8c85aff84a6817fe616af0e
Thomas Cannon <tcannon.mail@gmail.com>
2023-03-16 17:21:00 -0500

    performance: migrated from numpy to pandas because datarames are easier to work with

--- grbpy/batse.py
+++ grbpy/batse.py
@@ -1,6 +1,5 @@
-import os
 import sys
-import numpy as np
+import pandas as pd
 
 
 class BATSEBurst:
@@ -12,24 +11,37 @@ class BATSEBurst:
         else:
             sys.exit(f'file_type must be one of: {accepted_time_signatures}')
         self.file_path = file_path
+        self.meta_data = None
         self.header_names = None
         self.header_data = None
+        self.temp_meta_names = None
+        self.temp_meta_data = None
         self.raw_data = None
-        self.ind_chan_data = None
-        self.sum_chan_data = None
+        self.chan_data = None
 
     def parse_file(self):
         f = open(self.file_path, 'r')
         if self.time_signature == '64ms':
-            self.header_names = f.readline()
-            self.header_data = f.readline()
+
+            self.temp_meta_names = f.readline().split()[:4]
+            self.temp_meta_data = f.readline().split()[:4]
+            meta_dict = {}
+            for i in range(len(self.temp_meta_names)):
+                meta_dict[self.temp_meta_names[i]] = int(self.temp_meta_data[i])
+            self.meta_data = meta_dict
+            del self.temp_meta_data
+            del self.temp_meta_names
+
             self.raw_data = f.read()
-            self.ind_chan_data = np.genfromtxt(self.file_path, delimiter='  ', skip_header=2)
-            self.sum_chan_data = self.ind_chan_data.sum(axis=1)
+            self.header_names = ['chan1', 'chan2', 'chan3', 'chan4']
+            self.chan_data = pd.read_csv(self.file_path, header=2, delimiter=r"\s+")
+            self.chan_data.columns = self.header_names
+            self.chan_data['sum_chan'] = self.chan_data[list(self.chan_data.columns)].sum(axis=1)
 
     def summary(self, raw=False):
         if self.time_signature == '64ms':
-            print(f'Header: \n{self.header_names}{self.header_data}')
+            print(f'Meta Data: \n{self.meta_data}')
+            print(f'Data: \n{self.chan_data}')
             if raw:
                 print(f'Raw Header: \n{self.header_names}{self.header_data}')
                 print(f'Raw data: \n{self.raw_data}')
@@ -47,9 +59,11 @@ class BATSEDurations:
         f = open(self.file_path, 'r')
         self.header_names = ['trig_num', 't50', 't50e', 't50_start', 't90', 't90e', 't90_start']
         self.raw_data = f.read()
-        self.dur_data = np.genfromtxt(self.file_path)
+        self.dur_data = pd.read_csv(self.file_path, header=None, delimiter=r"\s+")
+        self.dur_data.columns = self.header_names
 
     def summary(self, raw=False):
         print(f'See BATSE.md for Summary')
+        print(self.dur_data)
         if raw:
             print(f'Raw data: \n{self.raw_data}')
--- test_burst.py
+++ test_burst.py
@@ -3,10 +3,10 @@ from grbpy.batse import BATSEBurst, BATSEDurations
 burst_file_path = 'sample_data/cat64ms.00143'
 test_burst = BATSEBurst(file_path=burst_file_path, time_signature='64ms')
 test_burst.parse_file()
-test_burst.summary(raw=True)
+test_burst.summary(raw=False)
 
 
 duration_file_path = 'sample_data/duration_table.txt'
 dur_table = BATSEDurations(file_path=duration_file_path)
 dur_table.parse_file()
-dur_table.summary(raw=True)
+dur_table.summary(raw=False)
